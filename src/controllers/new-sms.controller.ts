95c53601d6150a5e2e12acc71bba88f7ecb1e583
Antony Noyelle <anto75019@gmail.com>
2020-12-20 20:07:11 +0100

    notif newSms

--- src/controllers/new-sms.controller.ts
+++ src/controllers/new-sms.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Get, Post, Body, HttpService } from '@nestjs/common';
+import { outbox } from 'src/entities/outbox.entity';
+import { environment } from 'src/environment/environment';
 
 @Controller('new-sms')
 export class NewSmsController {
@@ -11,7 +13,7 @@ export class NewSmsController {
         console.log("-*--*-*-*-*-*- newSms UPDATE")
     }
     @Post()
-    newSmsInfos(@Body() sms:any){
+    newSmsInfos(@Body() sms:outbox){
         console.log("-*--*-*-*-*-*- newSms UPDATE INFOS -> ", sms)
         return this.sendNotifToAdmin(sms)
     }
@@ -25,6 +27,10 @@ export class NewSmsController {
             "timestamp":Date.now(),
             "icon":"assets/icons/icon-152x152.png"
         }
-        return this.http.post("http://192.168.1.15:1901/notification/user/1", notif)
+        return this.http.post("http://192.168.1.15:1901/notification/user/1", notif, {
+            headers: {
+              'noyeau-api-key': environment.apiKeyCode,
+              'content-type': "application/json"
+            }})
     }
 }
